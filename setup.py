312e05ae0fac2df03d20012dcc51fc26b385a1ad
fujihiraryo <fujihiraryo.team-lab.com>
2021-03-25 03:14:42 +0900

    add dist

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(
+    name="tweet",
+    version="0.0.1",
     entry_points={
         "console_scripts": [
             "tweet=tweet.app:tweet",
         ]
-    }
+    },
 )
